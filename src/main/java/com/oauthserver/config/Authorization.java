8402730797209109f933ad8c7260a99f77a93146
이승환 <lshk955@naver.com>
2020-04-14 17:31:37 +0900

    수정및 리펙터링

--- src/main/java/com/oauthserver/config/Authorization.java
+++ src/main/java/com/oauthserver/config/Authorization.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 /**
  * @author 이승환
